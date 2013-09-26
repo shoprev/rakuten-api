185838208d0afc5ef0d8f2def062ff7e429e793f
shoprev <admin@shoprev.net>
2013-09-26 13:29:34 +0900

    v0.0.1

--- example/ichiba.rb
+++ example/ichiba.rb
@@ -1,6 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper')
 
 # Ichiba Item Search API (version:2013-08-05)
-# res = Rakuten::Api.get(Rakuten::Api::Ichiba::Search,{:genreId => "101240"})
-# res = Rakuten::Api::Ichiba.search({:genreId => "101240"})
-# p res
+res = Rakuten::Api.get(Rakuten::Api::Ichiba::Search,{:genreId => "101240"})
+res["Items"].each do |v|
+  p v["Item"]["itemName"]
+end
+
+# Ichiba Genre Search API (version:2012-07-23)
+res = Rakuten::Api.get(Rakuten::Api::Ichiba::GenreSearch,{:genreId => "101240"})
+res["children"].each do |v|
+  p v["child"]["genreName"]
+end
+
+# Ichiba Item Ranking API (version:2012-09-27)
+res = Rakuten::Api.get(Rakuten::Api::Ichiba::Ranking,{:genreId => "101240"})
+res["Items"].each do |v|
+  p v["Item"]["itemName"]
+end
